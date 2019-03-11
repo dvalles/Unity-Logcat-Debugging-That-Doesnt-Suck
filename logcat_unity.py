f843e1dd62ac533286f0c83f6c4f7fb9a665a5c2
Dom <domvalles@gmail.com>
2019-03-11 12:09:03 -0400

    Add the filter file

---
+++ logcat_unity.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+#
+# Usage:
+# adb logcat -v time | python logcat_unity.py
+
+import sys
+import re
+import colorama
+
+colorama.init()
+
+IGNORED_LINES=["(Filename:"]
+LOG_TAG="Unity"
+REGEX_LINE = "([0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9]) ([DIWEF])\/"+LOG_TAG+"\s*\((\s*[0-9]*)\):(.*)"
+
+RED="\033[1;31m"
+YELLOW="\033[1;33m"
+NO_COLOR="\033[0m"
+
+def colorize(color, text):
+	return color + text + NO_COLOR
+
+def isDebugOrInfo(log_level):
+	log_level = log_level.rstrip()
+	return log_level=="I" or log_level=="D"
+
+def isWarningOrInfo(log_level):
+	log_level = log_level.rstrip()
+	return log_level=="I" or log_level=="W"
+
+def isException(log_message):
+	containsException = (log_message.find("Error") != -1) or (log_message.find("Exception") != -1)
+	return containsException or log_message.strip().startswith("at ")
+
+def isIgnored(log_message):
+	for ignored in IGNORED_LINES:
+		if ignored in log_message:
+			return True
+	return False
+
+while True:
+	line = sys.stdin.readline().rstrip()
+	match = re.search(REGEX_LINE, line)
+
+	if not match:
+		continue
+
+	time = match.group(1)
+	log_level = match.group(2).strip()
+	process_id = match.group(3)
+	log_message = match.group(4)
+
+	if not log_message:
+		continue
+
+	if isIgnored(log_message):
+		continue
+
+	if isWarningOrInfo(log_level): #I don't want warning or info messages, you could change this easily
+		continue
+
+	# output = time + " " + log_level + " " + log_message
+	output = log_level + " " + log_message #don't need timestamp
+	if isException(log_message):
+		print(colorize(RED, output))		
+	elif not isDebugOrInfo(log_level):
+		print(colorize(YELLOW, output))
+	else:
+		print(output)
